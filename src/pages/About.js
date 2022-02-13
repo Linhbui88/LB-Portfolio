3f3412a427e91df68eb354a64890ee07db398f80
LinhBui88 <87442413+Linhbui88@users.noreply.github.com>
2022-02-12 20:45:03 -0500

    adding About Me

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,10 +8,8 @@ const About = () => {
         <h2>ABOUT ME</h2>
       </div>
       <div className="section-container">
-        I'm a fullstack web developer with background in Graphic Design and recently graduated from the <span>MSU
-        Coding Boot Camp</span>. I am passionate about coding and solving problems
-        through code, and I am excited to work alongside other amazing
-        programmers and learn so much more!
+        I'm a fullstack web developer with background in Graphic Design and recently graduated from the <span>Michigan State University
+        Coding Boot Camp</span>. I am passionate about coding and solving problems through code, as well as bulding beautiful, functional and fast web application.
       </div>
     </div>
   );
